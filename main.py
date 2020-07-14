8302fa2c4d297aa3e42478051f4545eca464f6f5
Ronanlxck <robertsregime@gmail.com>
2020-07-14 16:58:20 +0000

    everything

---
+++ main.py
@@ -0,0 +1,9 @@
+
+a = "My name is Ronan Roberts"
+b = "and my favorite musicain is xxxtentacion"
+c = "One of the places i would love to go to is California"
+d = "One of my hobbies is playing football and going outside"
+print (a)
+print (b)
+print (c)
+print(d)
